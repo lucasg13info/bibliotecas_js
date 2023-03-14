8a9bc265c14fadf7d01a8536dfe242f190157053
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-03-14 00:32:01 -0300

    Criado o array de objetos e iterado no fron

--- dom.js
+++ dom.js
@@ -30,5 +30,38 @@ lista.innerHTML = `<p class="editarParagrafo">${texto1}</p>
         document.getElementById("newUser").value;
         let novoUser = prompt("Insira o nome do novo usuário")
 
-        alert("Inserido o novo usuário: " + novoUser)
+        if (novoUser != ''){
+            alert("Inserido o novo usuário: " + novoUser)
+
+        }else{
+            let novoUser = alert("Você não inseriu o nome do usuário, tente novamente.")
+        }
+
+        
     }
+
+
+
+
+//Array de objetos:
+let filmeLista = [
+    { 
+        id: 0,
+        nomeFilme: "VINGADORES: ULTIMATO", 
+        genero: "Ação", 
+        duracao: "2h 03min",
+        sinopse: "Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark (Robert Downey Jr.) vagando perdido no espaço sem água nem comida, o Capitão América/Steve Rogers (Chris Evans) e a Viúva Negra/Natasha Romanov (Scarlett Johansson) precisam liderar a resistência contra o titã louco.",
+        lancamento: "11 de abril de 2019",
+        capa: "https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg"
+    
+    }]
+
+
+
+for (let filme of filmeLista){
+    let caixa = document.getElementById("listaFilmes")
+    caixa.innerHTML = `<h3 class='tituloh3'>${filme.nomeFilme}</h3>
+                       <img class='imgs' src='${filme.capa}'>
+                       <p> Duração: ${filme.duracao}</p>
+                       <p> Sinopse: ${filme.sinopse}</p>`
+}
